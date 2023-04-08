55ad21c17c62c39e0ade1cad2597c8080ac51f02
Maciej <mpiatek1512@gmail.com>
2023-04-08 12:44:10 +0200

    Reading file

--- src/main/java/org/sorter/Main.java
+++ src/main/java/org/sorter/Main.java
@@ -1,7 +1,17 @@
 package org.sorter;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try(Scanner sc = new Scanner(new File("src/main/resources/numbers.txt"))){
+            while(sc.hasNextInt()){
+                System.out.println(sc.nextInt());
+            }
+        }catch (FileNotFoundException e){
+            System.out.println("File not found");
+        }
     }
 }
